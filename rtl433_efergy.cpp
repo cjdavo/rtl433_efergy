862a5032fd01308e5be8104129086812db85b5c4
AusGadgets <chris.davidson@cantrayit.com.au>
2024-09-16 19:33:30 +0800

    Update rtl433_efergy.cpp

--- rtl433_efergy.cpp
+++ rtl433_efergy.cpp
@@ -1,1 +1,10 @@
+#include "rtl433-to-mqtt.h"
+
+void Rtl433ToMqtt::setup() {
+  ESP_LOGD("Rtl433ToMqtt", "Setup completed");
+}
+
+void Rtl433ToMqtt::loop() {
+  // Logic to read and process MQTT or sensor data
+}
 
