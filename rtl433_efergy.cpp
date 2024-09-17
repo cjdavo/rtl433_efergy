acbb44913c50001bf7a06bd417367e702a9a1a11
AusGadgets <chris.davidson@cantrayit.com.au>
2024-09-17 21:39:22 +0800

    Update rtl433_efergy.cpp

--- rtl433_efergy.cpp
+++ rtl433_efergy.cpp
@@ -1,3 +1,7 @@
+namespace esphome {
+namespace rtl433_efergy_component {
+
+static const char *TAG = "rtl433_efergy_component.component";
 #include "rtl433-to-mqtt.h"
 
 void Rtl433ToMqtt::setup() {
@@ -7,4 +11,5 @@ void Rtl433ToMqtt::setup() {
 void Rtl433ToMqtt::loop() {
   // Logic to read and process MQTT or sensor data
 }
-
+}  // namespace empty_uart_component
+}  // namespace esphome
